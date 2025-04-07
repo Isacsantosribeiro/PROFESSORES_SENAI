0eaf1ce2d29a05dfbc5af27680460099c63b2601
sistemanoturno <sistemanoturno@LAB5-08.sistemafieto.rede>
2025-04-07 20:09:15 -0300

    Tela cadastro Instrutor

---
+++ GESTAOOCIOSIDADE/src/Controller/ControllerCadastroInstrutor.java
@@ -0,0 +1,33 @@
+package Controller;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ControllerCadastroInstrutor {
+
+    @FXML
+    private Button btnCancelar;
+
+    @FXML
+    private Button btnSalvar;
+
+    @FXML
+    private TextField txtCPF;
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    void actionCadastrar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionCancelar(ActionEvent event) {
+
+    }
+
+}
